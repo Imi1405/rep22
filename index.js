73e132c86048f8befb3cff4eb4b87ee156d68e97
Imi1405 <kh.imen88@gmail.com>
2018-02-12 22:01:44 +0100

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,77 @@
+
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const bodyparser = require('body-parser');
 const app = express();
+const ObjectId = require('mongodb').ObjectID;
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.get('/',function(request, response){
-    response.send('Hello FiveP');
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017/app1db', (err, client) => {
+        if (err) return console.log(err);
+        let db = client.db('app1db');
+        closure(db);
+    })
+}
+
+app.get('/users', (req, res) => {
+    connection(db => {
+        db.collection('test').find().toArray((err, result) => {
+            res.send(result);
+        })
+    })
 });
 
-app.post('/',(req,res)=>{
+app.get('/users/:id/:iTodo', (req, res) => {
+    let userID = req.params.id;
+    let indexTodo = req.params.iTodo;
+    connection(db => {
+        db.collection('test').findOne({ _id: ObjectId(userID) }, (err, result) => {
+            if (err) throw err;
+            res.send(result.todos[indexTodo]);
+
+        })
+    })
+
+});
+
+app.put('/users/:id/:iTodo', (req, res) => {
+    let userId = req.params.id;
+    let indexTodo = req.params.iTodo;
     console.log(req.body);
-    res.send('i got some data');
+    connection(db => {
+        db.collection('test').update({ _id: ObjectId(userId) },
+            { $set: { "todos.1": req.body } },
+            (err, result) => {
+                res.send(result);
+            })
+    })
+})
+
+app.delete('/', (req, res) => {
+    connection(db => {
+        db.collection('pool').deleteOne(req.body, (err, result) => {
+
+            if (err) throw err;
+            res.send(result);
+            console.log('1 document deleted');
+            db.close();
+        });
+    });
+});
+
+
+
+
+app.put('/', (req, res) => {
+    res.send({datareceived:req.body})
+    // connection(db => {
+    //     db.collection('test').insert(req.body, (err, result) => {
+    //         if (err) throw err;
+    //         res.send(result);
+    //     })
+    // })
 });
 
 console.log('hani nasma3 fik 3al port 3000');
